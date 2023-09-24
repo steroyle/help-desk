b491c2f40e213bf8a9df885a8a014d52ad7ea6c9
steroyle <steroyle@gmail.com>
2023-09-24 16:05:56 +0100

    Added ticket details page

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const getTickets =  async () => {
   const res = await fetch('http://localhost:4000/tickets', {
     next: {
@@ -14,11 +16,13 @@ const TicketList = async () => {
     <>
       {tickets.map(ticket => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
