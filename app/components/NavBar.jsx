151596f22e8861f2701dac0eb215e514b9052a2f
steroyle <steroyle@gmail.com>
2023-09-24 01:39:20 +0100

    Added logo

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./help-desk-logo.png";
 
 const NavBar = () => {
   return (
     <nav>
+      <Image src={Logo} alt="Help desk logo" width={50} quality={100} />
       <h1>Help Desk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
