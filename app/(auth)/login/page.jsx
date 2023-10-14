7319a7066a1c47f72e63d882a2187a2d9c7fe0b4
steroyle <steroyle@gmail.com>
2023-10-14 22:29:19 +0100

    Added route groups a restructured

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <main>
+      <h2 className="text-center">Login</h2>
+    </main>
+  )
+}
+
+export default Login;
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,9 @@
+const Signup = () => {
+  return (
+    <main>
+      <h2 className="text-center">Sign up</h2>
+    </main>
+  )
+}
+
+export default Signup;
